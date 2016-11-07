63f22241eec060633af088d4deb094f7f7084beb
Adal <avengerdragon@gmail.com>
2016-11-07 14:49:10 +0000

    Moved VivePawn logic to ViveManipulator

---
+++ Assets/New Scripts/ViveManipulator.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.New_Scripts
+{
+    public class ViveManipulator
+    {
+        private GameObject vivePawn;
+        private GameObject lastCollided;
+        private GameObject prevCollided;
+
+        // for scaling JFG
+        private int quadrantWorld;
+        private int quadrantObject;
+        private float scaleFactor = 1.0f;
+
+        public Vector3 PrevPosition { get; set; }
+        public Vector3 CurrentPosition { get; set; }
+        public GameObject ManipulatedObject { get; set; }
+        public InteractionMode InteractionMode { get; set; }
+
+        public ViveManipulator(GameObject vivePawn)
+        {
+            this.vivePawn = vivePawn;
+        }
+
+        public void DragObject()
+        {
+            Debug.Log("DragObject - mode:" + InteractionMode);
+            if (InteractionMode != InteractionMode.Manipulation)
+                return;
+
+            if (lastCollided == null)
+                return;
+            ManipulatedObject = lastCollided;
+
+            ManipulatedObject.transform.parent = vivePawn.transform;
+
+            Debug.Log("Manipulating:" + lastCollided.name);
+        }
+
+        public void ReleaseObject()
+        {
+            Debug.Log("Releasing:" + ManipulatedObject.name);
+            if (ManipulatedObject != null)
+            {
+                ManipulatedObject.transform.parent = null;
+                ManipulatedObject = null;
+            }
+        }
+
+        public void ChangeMode()
+        {
+            switch (InteractionMode)
+            {
+                case InteractionMode.None:
+                    InteractionMode = InteractionMode.Manipulation;
+                    break;
+
+                case InteractionMode.Manipulation:
+                    InteractionMode = InteractionMode.ScalePrefabs;
+
+                    break;
+
+                //case InteractionMode.SpawnPrimitives:
+                //    InteractionMode = InteractionMode.Manipulation;
+                //    break;
+
+                case InteractionMode.ScalePrefabs:
+                    InteractionMode = InteractionMode.Manipulation;
+                    break;
+            }
+
+            Debug.Log("InteractionMode: " + InteractionMode);
+        }
+
+        public void ChangeColor()
+        {
+            Color newColor = Color.black;
+            switch (InteractionMode)
+            {
+                case InteractionMode.None:
+                    newColor = Color.red;
+                    break;
+
+                case InteractionMode.Manipulation:
+                    newColor = Color.green;
+                    break;
+                case InteractionMode.SpawnPrimitives:
+                    newColor = Colors.Gold;
+                    break;
+
+                case InteractionMode.ScalePrefabs:
+                    newColor = Color.black;
+                    break;
+            }
+            foreach (var meshRender in vivePawn.GetComponentsInChildren<MeshRenderer>())
+                meshRender.material.color = newColor;
+        }
+
+        public void ScaleObject()
+        {
+            
+            if (InteractionMode != InteractionMode.ScalePrefabs)
+                return;
+
+            if (ManipulatedObject == null)
+                return;
+            var deltaP = CurrentPosition - PrevPosition;
+            Debug.Log("delta: " + deltaP);
+
+            if (deltaP.magnitude >= 10)
+            {
+                CurrentPosition = vivePawn.transform.position;
+                return;
+            }
+
+            quadrantWorld = QuadrantFromVector(new Vector3(0, 0, 1));
+            quadrantObject = QuadrantFromVector(vivePawn.transform.forward.normalized);
+
+            Debug.Log("Quadrantword" + quadrantWorld);
+            switch (quadrantWorld)
+            {
+                case 1:
+
+                    switch (quadrantObject)
+                    {
+
+                        case 1:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, -deltaP.z) / scaleFactor;
+                            break;
+                        case 2:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
+                            break;
+                        case 3:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
+                            break;
+                        case 4:
+                            ManipulatedObject.transform.localScale += new Vector3(-deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    switch (quadrantObject)
+                    {
+                        case 1:
+                            ManipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
+                            break;
+                        case 2:
+                            ManipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
+                            break;
+                        case 3:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
+                            break;
+                        case 4:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
+                            break;
+                    }
+                    break;
+
+                case 3:
+                    switch (quadrantObject)
+                    {
+                        case 1:
+                            ManipulatedObject.transform.localScale += new Vector3(-deltaP.x, deltaP.y, +deltaP.z) / scaleFactor;
+                            break;
+                        case 2:
+                            ManipulatedObject.transform.localScale += new Vector3(-deltaP.x, deltaP.y, -deltaP.z) / scaleFactor;
+                            break;
+                        case 3:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, -deltaP.z) / scaleFactor;
+                            break;
+                        case 4:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
+                            break;
+                    }
+                    break;
+
+
+
+                case 4:
+                    switch (quadrantObject)
+                    {
+                        case 1:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
+                            break;
+                        case 2:
+                            ManipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
+                            break;
+                        case 3:
+                            ManipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
+                            break;
+                        case 4:
+                            ManipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
+                            break;
+                    }
+                    break;
+
+
+            }
+
+            Debug.Log("Manipulating:" + lastCollided.name);
+        }
+
+        int QuadrantFromVector(Vector3 axisForward)
+        {
+            var angle = SignedAngle(axisForward, lastCollided.transform.forward.normalized);
+
+            if (angle >= 45 && angle < 135)
+                return 2;
+            else if (angle < -45 && angle > -135)
+                return 4;
+            else if ((angle >= 135 && angle <= 180) || (angle < -135 && angle >= -180))
+                return 3;
+            else if ((angle >= 0 && angle < 45) || (angle > -45 && angle < 0))
+                return 1;
+            else return -1;
+        }
+
+        private float SignedAngle(Vector3 viewForward, Vector3 objForward)
+        {
+            // the vector that we want to measure an angle from
+
+            Vector3 referenceRight = Vector3.Cross(Vector3.up, viewForward);
+            // the vector of interest
+
+            float angle = Vector3.Angle(objForward, viewForward);
+            // Determine if the degree value should be negative.  Here, a positive value
+            // from the dot product means that our vector is on the right of the reference vector   
+            // whereas a negative value means we're on the left.
+            float sign = Mathf.Sign(Vector3.Dot(objForward, referenceRight));
+            float finalAngle = sign * angle;
+            return finalAngle;
+        }
+
+
+        public void CheckHits(Vector3 controllerPosition, Vector3 controllerForward)
+        {
+            RaycastHit hitInfo;
+            var sphere = vivePawn.transform.Find("raySphere").gameObject;
+
+            if (Physics.Raycast(new Ray(controllerPosition, controllerForward), out hitInfo))
+            {
+                prevCollided = lastCollided;
+                lastCollided = hitInfo.transform.gameObject;
+                sphere.transform.position = hitInfo.point;
+                sphere.GetComponent<MeshRenderer>().enabled = true;
+
+            }
+            else
+            {
+                sphere.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+
+        public void CaptureCollided()
+        {
+            Debug.Log("DragObject - mode:" + InteractionMode);
+            if (lastCollided != null)
+                ManipulatedObject = lastCollided;
+        }
+
+    }
+}
--- Assets/New Scripts/VivePawn.cs
+++ Assets/New Scripts/VivePawn.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Linq;
 using Assets.New_Scripts;
 using UnityEngine.Networking;
 
@@ -9,26 +7,17 @@ public class VivePawn : NetworkBehaviour
     public ViveBridge ViveBridge;
     private GameObject rayMesh;
     private MeshRenderer rayMeshRenderer;
+   
 
-    private GameObject lastCollided;
-    private GameObject prevCollided;
-    private GameObject manipulatedObject;
-    private bool isDragging;
-    [SyncVar]
-    private InteractionMode interactionMode;
-
-    // for scaling JFG
-    Vector3 currentPosition;
-    private Vector3 prevPosition;
-    private int quadrantWorld;
-    private int quadrantObject;
-    private float scaleFactor = 1.0f;
+    private ViveManipulator viveManipulator;
+   
     private bool IsScaling = false;
 
     // Use this for initialization
     void Start()
     {
         ViveBridge = GameObject.Find("ViveBridge").GetComponent<ViveBridge>();
+        viveManipulator = new ViveManipulator(gameObject);
 
         rayMesh = GetComponentInChildren<MeshRenderer>().transform.parent.gameObject;
 
@@ -45,6 +34,7 @@ public class VivePawn : NetworkBehaviour
     {
         if (!isLocalPlayer)
             return;
+
         Debug.Log("Ungripped");
         SpawnFactory.Spawn("Prefabs/TestPrefab", Vector3.zero, Quaternion.identity);
     }
@@ -58,54 +48,39 @@ public class VivePawn : NetworkBehaviour
     [ClientRpc]
     private void RpcChangeMode()
     {
-        switch (interactionMode)
-        {
-            case InteractionMode.None:
-                interactionMode = InteractionMode.Manipulation;
-                IsScaling = false;
-                break;
+        viveManipulator.ChangeMode();
+    }
 
-            case InteractionMode.Manipulation:
-                interactionMode = InteractionMode.ScalePrefabs;
-                break;
-
-            /*
-        case InteractionMode.SpawnPrimitives:
-            interactionMode = InteractionMode.Manipulation;
-            break;
-            */
-
-            case InteractionMode.ScalePrefabs:
-                interactionMode = InteractionMode.Manipulation;
-                break;
-
-        }
-
-        Debug.Log("InteractionMode: " + interactionMode.ToString());
+    [ClientRpc]
+    private void RpcCapture()
+    {
+        viveManipulator.CaptureCollided();
     }
 
     private void ViveBridge_TriggerClicked(object sender, ClickedEventArgs e)
     {
         Debug.Log("TriggerClicked");
-        // RpcDragObject();
-        switch (interactionMode)
+        
+        switch (viveManipulator.InteractionMode)
         {
             case InteractionMode.Manipulation:
                 RpcDragObject();
                 break;
 
             case InteractionMode.ScalePrefabs:
-                //RpcScaleObject();
-                IsScaling = true;
+                RpcCapture();
                 break;
         }
     }
 
+
     private void ViveBridge_TriggerUnclicked(object sender, ClickedEventArgs e)
     {
         Debug.Log("TriggerUnclicked");
-        switch (interactionMode)
+
+        switch (viveManipulator.InteractionMode)
         {
+            case InteractionMode.ScalePrefabs:
             case InteractionMode.Manipulation:
                 RpcReleaseObject();
                 break;
@@ -114,11 +89,9 @@ public class VivePawn : NetworkBehaviour
                 SpawnFactory.Spawn("Prefabs/SphereMarker", transform.position, transform.rotation);
                 break;
 
-                case InteractionMode.ScalePrefabs:
-                manipulatedObject = null;
-                break;
         }
     }
+
 
     // Update is called once per frame
     void Update()
@@ -126,16 +99,17 @@ public class VivePawn : NetworkBehaviour
         if (!isLocalPlayer)
             return;
 
-        prevPosition = transform.position;
+        viveManipulator.PrevPosition = transform.position;
 
         transform.position = ViveBridge.Position;
         transform.rotation = ViveBridge.Rotation;
         rayMesh.transform.rotation = ViveBridge.Rotation;
-        currentPosition = transform.position;
+
+        viveManipulator.CurrentPosition = transform.position;
 
         CheckHits();
 
-        if (IsScaling)
+        if (viveManipulator.InteractionMode == InteractionMode.ScalePrefabs)
         {
             RpcScaleObject();
         }
@@ -147,73 +121,18 @@ public class VivePawn : NetworkBehaviour
     {
         if (!isLocalPlayer)
             return;
-        RaycastHit hitInfo;
-        var sphere = transform.Find("raySphere").gameObject;
-
-        if (Physics.Raycast(new Ray(ViveBridge.Position, ViveBridge.Forward), out hitInfo))
-        {
-            prevCollided = lastCollided;
-            lastCollided = hitInfo.transform.gameObject;
-            sphere.transform.position = hitInfo.point;
-            sphere.GetComponent<MeshRenderer>().enabled = true;
-
-            if (IsScaling && manipulatedObject == null)
-                manipulatedObject = lastCollided;
-        }
-        else
-        {
-            sphere.GetComponent<MeshRenderer>().enabled = false;
-        }
         
+        viveManipulator.CheckHits(ViveBridge.Position, ViveBridge.Forward);
     }
 
-    private float signedAngle(Vector3 viewForward, Vector3 objForward)
-    {
-        // the vector that we want to measure an angle from
-
-        Vector3 referenceRight = Vector3.Cross(Vector3.up, viewForward);
-        // the vector of interest
-
-        float angle = Vector3.Angle(objForward, viewForward);
-        // Determine if the degree value should be negative.  Here, a positive value
-        // from the dot product means that our vector is on the right of the reference vector   
-        // whereas a negative value means we're on the left.
-        float sign = Mathf.Sign(Vector3.Dot(objForward, referenceRight));
-        float finalAngle = sign * angle;
-        return finalAngle;
-    }
-
-    int QuadrantFromVector(Vector3 axisForward)
-    {
-        var angle = signedAngle(axisForward, lastCollided.transform.forward.normalized);
-
-        if (angle >= 45 && angle < 135)
-            return 2;
-        else if (angle < -45 && angle > -135)
-            return 4;
-        else if ((angle >= 135 && angle <= 180) || (angle < -135 && angle >= -180))
-            return 3;
-        else if ((angle >= 0 && angle < 45) || (angle > -45 && angle < 0))
-            return 1;
-        else return -1;
-    }
 
     [ClientRpc]
     void RpcDragObject()
     {
         if (!isLocalPlayer)
             return;
-        Debug.Log("DragObject - mode:" + interactionMode);
-        if (interactionMode != InteractionMode.Manipulation)
-            return;
-
-        if (lastCollided == null)
-            return;
-        manipulatedObject = lastCollided;
-
-        manipulatedObject.transform.parent = transform;
-
-        Debug.Log("Manipulating:" + lastCollided.name);
+        
+        viveManipulator.DragObject();
     }
 
     [ClientRpc]
@@ -221,157 +140,22 @@ public class VivePawn : NetworkBehaviour
     {
         if (!isLocalPlayer)
             return;
-        if (interactionMode != InteractionMode.Manipulation)
-            return;
-        if (manipulatedObject != null)
-        {
-            manipulatedObject.transform.parent = null;
-            manipulatedObject = null;
-        }
+        viveManipulator.ReleaseObject();
     }
 
     [ClientRpc]
     void RpcChangeRayColor()
     {
-        Color newColor = Color.black;
-        switch (interactionMode)
-        {
-            case InteractionMode.None:
-                newColor = Color.red;
-                break;
-
-            case InteractionMode.Manipulation:
-                newColor = Color.green;
-                break;
-            case InteractionMode.SpawnPrimitives:
-                newColor = Colors.Gold;
-                break;
-        }
-        foreach (var meshRender in GetComponentsInChildren<MeshRenderer>())
-            meshRender.material.color = newColor;
+        viveManipulator.ChangeColor();
     }
 
-    [ClientRpc]
+    //[ClientRpc]
     void RpcScaleObject()
     {
         if (!isLocalPlayer)
             return;
-        Debug.Log("DragObject - mode:" + interactionMode);
-        if (interactionMode != InteractionMode.ScalePrefabs)
-            return;
-
-        //if (lastCollided == null)
-        //    return;
-
-        if (currentPosition == Vector3.zero)
-            return;
-
-        Debug.Log("lastposition" + currentPosition);
-        Debug.Log("transform" + transform.position);
-
-        var deltaP = currentPosition - prevPosition;
-        //currentPosition = transform.position;
-        //Debug.Log("deltaP" + deltaP);
-        if (deltaP.magnitude >= 10)
-        {
-            currentPosition = transform.position;
-            return;
-
-        }
-
-        quadrantWorld = QuadrantFromVector(new Vector3(0, 0, 1));
-        quadrantObject = QuadrantFromVector(transform.forward.normalized);
-
-        Debug.Log("Quadrantword" + quadrantWorld);
-        switch (quadrantWorld)
-        {
-            case 1:
-
-                switch (quadrantObject)
-                {
-
-                    case 1:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, -deltaP.z) / scaleFactor;
-                        break;
-                    case 2:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
-                        break;
-                    case 3:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
-                        break;
-                    case 4:
-                        manipulatedObject.transform.localScale += new Vector3(-deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
-                        break;
-                }
-                break;
-
-            case 2:
-                switch (quadrantObject)
-                {
-                    case 1:
-                        manipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
-                        break;
-                    case 2:
-                        manipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
-                        break;
-                    case 3:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
-                        break;
-                    case 4:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
-                        break;
-                }
-                break;
-
-            case 3:
-                switch (quadrantObject)
-                {
-                    case 1:
-                        manipulatedObject.transform.localScale += new Vector3(-deltaP.x, deltaP.y, +deltaP.z) / scaleFactor;
-                        break;
-                    case 2:
-                        manipulatedObject.transform.localScale += new Vector3(-deltaP.x, deltaP.y, -deltaP.z) / scaleFactor;
-                        break;
-                    case 3:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, -deltaP.z) / scaleFactor;
-                        break;
-                    case 4:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.x, deltaP.y, deltaP.z) / scaleFactor;
-                        break;
-                }
-                break;
-
-
-
-            case 4:
-                switch (quadrantObject)
-                {
-                    case 1:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
-                        break;
-                    case 2:
-                        manipulatedObject.transform.localScale += new Vector3(deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
-                        break;
-                    case 3:
-                        manipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, -deltaP.x) / scaleFactor;
-                        break;
-                    case 4:
-                        manipulatedObject.transform.localScale += new Vector3(-deltaP.z, deltaP.y, deltaP.x) / scaleFactor;
-                        break;
-                }
-                break;
-
-
-        }
-
-
-
-        Debug.Log("Manipulating:" + lastCollided.name);
-
-
+        viveManipulator.ScaleObject();
     }
-
-
 
     public override void OnStartLocalPlayer()
     {
